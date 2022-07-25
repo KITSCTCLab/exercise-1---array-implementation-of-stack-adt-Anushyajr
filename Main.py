232a8d90c2379cde74bda567de70672d59c60191
URK21CS3017 <109951732+URK21CS3017@users.noreply.github.com>
2022-07-25 15:26:17 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,15 @@
 import os
 class Stack:
     def __init__(self, size):
-        self.items = []
+        self.items = [None]*n
         self.size = size
         self.top=-1
 
     def is_empty(self):
-        return self.top==+1
+        return self.top==n+1
 
     def is_full(self):
-        return self.top==-1
+        return self.top==n-1
 
     def push(self, data):
         if not self.is_full():
