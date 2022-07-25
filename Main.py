58d1aaa92e75d63e522eba6f6731b997ceb43f58
URK21CS3017 <109951732+URK21CS3017@users.noreply.github.com>
2022-07-25 15:21:10 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,16 +3,17 @@ class Stack:
     def __init__(self, size):
         self.items = []
         self.size = size
+        self.top=-1
 
     def is_empty(self):
-        return self.size==+1
+        return self.top==+1
 
     def is_full(self):
-        return self.size==-1
+        return self.top==-1
 
     def push(self, data):
         if not self.is_full():
-            self=size
+            self=top
             x=int(input("enter the data to be inserted:")
                   self.item[self.size]=x
 
